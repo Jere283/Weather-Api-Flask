d9c847eba121d34b03d49a23a7fcc4e081de7fc1
Jere283 <jeremyfigueroa2015@outlook.com>
2023-09-06 22:21:39 -0600

    Api returns data of the station with a year and all the years in the station

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from flask import Flask, render_template
 import pandas as pd
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+    stations = stations[['STAID', "STANAME                                 "]]
+    return render_template("index.html", data= stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
-def runApi(station, date):
+def run_api(station, date):
     filename = "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
     return {"station": station, "date": date, "temperature": temperature}
 
+
+@app.route("/api/v1/<station>")
+def all_date(station):
+    filename = "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    data = df.to_dict(orient="records")
+    return data
+
+
+@app.route("/api/v1/annual/<station>/<year>")
+def yearlyDate(station, year):
+    filename = "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    data = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
